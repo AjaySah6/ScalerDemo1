73434e04166a1dd3cf1ee1b2701a3027b20cf2f4
Ajay Sahe <ajaysah884@gmail.com>
2024-09-26 07:43:44 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/dev_ajay/scalerdemo1/Models/Category.java
    #	src/main/java/com/dev_ajay/scalerdemo1/Repositories/CategoryRepository.java

--- src/main/java/com/dev_ajay/scalerdemo1/Repositories/ProductRepository.java
+++ src/main/java/com/dev_ajay/scalerdemo1/Repositories/ProductRepository.java
@@ -2,10 +2,24 @@ package com.dev_ajay.scalerdemo1.Repositories;
 
 import com.dev_ajay.scalerdemo1.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product save(Product product);
     Product findById(long id);
     List<Product> findAll();
+
+    // HQL
+    @Query("select p from Product p where p.category.title = :title and p.id = :id")
+    Product getProductWithASpecificTitleAndId(@Param("title") String title,
+                                              @Param("id") Long id);
+
+    // Native SQL query
+    @Query(value = "select * from product where id = :id and title = :title", nativeQuery = true)
+    Product getProductWithSomeTitleAndId(@Param("title") String title,
+                                         @Param("id") Long id);
 }
--- src/test/java/com/dev_ajay/scalerdemo1/ScalerDemo1ApplicationTests.java
+++ src/test/java/com/dev_ajay/scalerdemo1/ScalerDemo1ApplicationTests.java
@@ -1,10 +1,27 @@
 package com.dev_ajay.scalerdemo1;
 
-//@SpringBootTest
-class ScalerDemo1ApplicationTests {
-    // No tests here
+import com.dev_ajay.scalerdemo1.Models.Product;
+import com.dev_ajay.scalerdemo1.Repositories.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//    @Test
-//     void contextLoads() { }
+@SpringBootTest // Needed dependency -> spring-boot-starter-test
+public class ScalerDemo1ApplicationTests {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void testingQuery() {
+        // Assuming there's a product with a specific title and id in your database
+        Product product = productRepository.getProductWithSomeTitleAndId("iPhone XR", 1L);
+
+        // Print the price of the product if found
+        if (product != null) {
+            System.out.println(product.getPrice());
+        } else {
+            System.out.println("Product not found.");
+        }
+    }
 }
